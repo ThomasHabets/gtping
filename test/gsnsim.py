5b6c1c535c9b283fe63928d8320a74865f9cbe22
Thomas Habets <thomas@habets.pp.se>
2010-06-01 23:20:20 +0200

    ugliest cmdline parser ever added to test GTP ping responder

--- test/gsnsim.py
+++ test/gsnsim.py
@@ -3,39 +3,19 @@
 #
 # Simulate a GSN node in that it responds to GTP pings in different ways.
 #
-import socket, struct, random, time
+import socket, struct, random, time, sys
+from threading import Thread
 
 def getPacket(fd):
-    data, src = fd.recvfrom(1048576)
+    data, src = fd.recvfrom(1000000)
     return data, src[:2]
-
-def getUnpack(fd):
-    """getUnpack(fd)
-
-    recvfrom() and unpack gtp packet.
-    """
-    data, src = fd.recvfrom(1048576)
-    src = src[0],src[1]
-    print "from %s port %d len %d" % (src[0], src[1], len(data))
-    return src, struct.unpack('cchihcc', data)
-
-def loopNormal(fd):
-    """loopNormal(fd)
-
-    Be a perfect gentleman and always return exactly one reply
-    """
-    while True:
-        packet, src = getPacket(fd)
-        reply = mkReply(packet)
-        fd.sendto(reply, src)
 
 def mkReply(req):
     ver = ord(struct.unpack('c', req[0])[0]) >> 5
-    print "Version: ",ver
     if ver == 1:
         flags,msg,ln,teid,seq,npdu,next = struct.unpack('cchihcc', req)
         if ord(msg) != 1:
-            return
+            return None
 
         return struct.pack('cchihcc',
                            flags,
@@ -48,6 +28,8 @@ def mkReply(req):
     elif ver == 2:
         if len(req) == 8:
             flags,msg,ln,seq,spare = struct.unpack('cchhh', req)
+            if ord(msg) != 1:
+                return None
             return struct.pack('cchhh',
                                flags,
                                chr(2),
@@ -56,6 +38,8 @@ def mkReply(req):
                                spare)
         elif len(req) == 12:
             flags,msg,ln,teid,seq,spare = struct.unpack('cchihh', req)
+            if ord(msg) != 1:
+                return None
             return struct.pack('cchihh',
                                flags,
                                chr(2),
@@ -68,25 +52,26 @@ def mkReply(req):
             raise "HELL"
 
 
-def loopFixed(fd, num = 2):
-    """loopFixed(fd, num = 2)
+def loopNormal(fd):
+    """loopNormal(fd)
+
+    Be a perfect gentleman and always return exactly one reply
+    """
+    while True:
+        packet, src = getPacket(fd)
+        reply = mkReply(packet)
+        fd.sendto(reply, src)
+
+
+def loopDup(fd, num = 2):
+    """loopDup(fd, num = 2)
 
     Always return num duplicates.
     """
     while True:
-        src, (flags,msg,ln,teid,seq,npdu,next) = getUnpack(fd)        
-        if ord(msg) != 1:
-            continue
+        packet, src = getPacket(fd)
         for n in range(num):
-            fd.sendto(struct.pack('cchihcc',
-                                  flags,
-                                  chr(2),
-                                  ln,
-                                  teid,
-                                  seq,
-                                  npdu,
-                                  next),
-                      src)
+            fd.sendto(mkReply(packet), src)
 
 def loopRandom(fd, minnum = 0, maxnum = 2):
     """loopRandom(fd, num = 2)
@@ -94,22 +79,10 @@ def loopRandom(fd, minnum = 0, maxnum = 2):
     Return between minnum and maxnum replies.
     """
     while True:
-        src, (flags,msg,ln,teid,seq,npdu,next) = getUnpack(fd)        
-        if ord(msg) != 1:
-            continue
+        packet, src = getPacket(fd)
         for n in range(random.randint(minnum,maxnum)):
-            fd.sendto(struct.pack('cchihcc',
-                                  flags,
-                                  chr(2),
-                                  ln,
-                                  teid,
-                                  seq,
-                                  npdu,
-                                  next),
-                      src)
+            fd.sendto(mkReply(packet), src)
         
-from threading import Thread
-
 packetscheduler = []
 class PacketScheduler(Thread):
     def __init__(self):
@@ -132,32 +105,23 @@ def loopJitter(fd, mintime=0, maxtime=1):
     ps = PacketScheduler()
     ps.start()
     while True:
-        src, (flags,msg,ln,teid,seq,npdu,next) = getUnpack(fd)        
-        if ord(msg) != 1:
-            continue
+        packet, src = getPacket(fd)
         packetscheduler.append( (time.time()
                                  + mintime
                                  + random.random() * (maxtime-mintime),
-                                 fd, src,
-                               struct.pack('cchihcc',
-                                           flags,
-                                           chr(2),
-                                           ln,
-                                           teid,
-                                           seq,
-                                           npdu,
-                                           next),
-                               )
-                              )
+                                 fd, src,mkReply(packet)) )
 
 def main():
     fd = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     fd.bind( ('', 2123) )
     try:
-        loopNormal(fd)
-        #loopDup(fd)
-        #loopRandom(fd)
-        #loopJitter(fd, mintime=0, maxtime=1)
+        {
+            'normal': loopNormal,
+            'dup': loopDup,
+            'random': loopRandom,
+            'jitter': lambda x: loopJitter(x, mintime=0, maxtime=1),
+         }.get(dict(zip(range(len(sys.argv)),sys.argv)).get(1, ''),
+                         loopNormal)(fd)
     except KeyboardInterrupt:
         fd.close()
 
